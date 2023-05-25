858b6bca26bcc10062eed696d16aace0fea2e041
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-05-25 04:12:47 -0300

    update-at

--- login-script.js
+++ login-script.js
@@ -1,16 +1,20 @@
+/** Carre as variaveis */
 const btnLogin = document.querySelector(".btn-login")
 const form = document.querySelector("form");
 
+/** Função 01 */
 btnLogin.addEventListener("click", event => {
     event.preventDefault();    
 
     form.classList.add("form-saida");
 });
+/** Função 02 */
 form.addEventListener("animationstart", event => {
     if(event.animationName === "down"){
         document.querySelector("body").style.overflow = "hidden"
     }
 })
+/** Função 03 */
 form.addEventListener("animationend", (event) => {
     if(event.animationName === "down"){
         // faz o form permanecer fora da tela ate recarregar
