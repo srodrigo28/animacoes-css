0e3d64943fff3b4ee5cdfe5ab512d371bebddb07
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-05-25 04:06:00 -0300

    update-finish-animate

--- login-script.js
+++ login-script.js
@@ -4,15 +4,13 @@ const form = document.querySelector("form");
 btnLogin.addEventListener("click", event => {
     event.preventDefault();    
 
-    form.classList.add("form-hide");
+    form.classList.add("form-saida");
 });
-
 form.addEventListener("animationstart", event => {
     if(event.animationName === "down"){
         document.querySelector("body").style.overflow = "hidden"
     }
 })
-
 form.addEventListener("animationend", (event) => {
     if(event.animationName === "down"){
         // faz o form permanecer fora da tela ate recarregar
